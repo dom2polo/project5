839a771ac19e1bd528cfbdf85aeb757525e4424b
Blake <83431780+blakehoosline@users.noreply.github.com>
2023-04-03 21:17:13 -0500

    Update tokenRing.cpp

--- tokenRing.cpp
+++ tokenRing.cpp
@@ -6,7 +6,8 @@ char buffer[BUFL] = {0};
 int sock = 0, valread;
 
 
-int requestChopsticks(int chopsticksIndex)
+// sends a request to get chopsticks and waits for a response from the server
+int requestChopsticks(int chopsticksIndex) // chopsticksIndex param is the index of the chopsticks to request
 {
     strcpy(buffer, "");
     switch (chopsticksIndex)
@@ -29,25 +30,31 @@ int requestChopsticks(int chopsticksIndex)
         break;
     }
 
+    // sends a request to get chopsticks
     send(sock, REQUESTCHOPSTICKS, strlen(REQUESTCHOPSTICKS), 0);
-
     printf("Requesting Chopsticks: %d\n", chopsticksIndex);
+    
+    // sends the index of the chopsticks to request
     send(sock, buffer, strlen(buffer), 0);
     printf("sending: %s\n", buffer);
 
     strcpy(buffer, "");
+    // will read the response from the server
     valread = read(sock, buffer, BUFL);
     printf("Server says: %s\n", buffer);
 
+    // if the response is OK the chopsticks are granted
     if (strcmp(buffer, "OK"))
         return true;
-    else if (strcmp(buffer, "NO"))
+    // if the response is NOT the chopsticks are not granted
+    else if (strcmp(buffer, "NOT"))
         return false;
 
     return 0;
 }
 
-int returnChopsticks(int chopsticksIndex)
+// sends a request to return chopsticks and waits for a response from the server
+int returnChopsticks(int chopsticksIndex) // chopsticksIndex param is the index of the chopsticks to return
 {
     switch (chopsticksIndex)
     {
@@ -70,16 +77,21 @@ int returnChopsticks(int chopsticksIndex)
     }
 
     printf("Returning Chopsticks: %d\n", chopsticksIndex);
+    
+    // sends request to return chopsticks
     send(sock, RETURNCHOPSTICKS, strlen(RETURNCHOPSTICKS), 0);
-
+    
+    // sends the index of the chopsticks to be returned
     send(sock, buffer, BUFL, 0);
-
+    
+    // reads the response from the server.
     valread = read(sock, buffer, BUFL);
     printf("Chopsticks Returned: %s\n", buffer);
 
     return 0;
 }
 
+// function simulates the philosopher eating
 void eat(struct philosopher phil)
 {
     phil.eatTime = rand() % TIMEMAX + 1;
@@ -87,6 +99,7 @@ void eat(struct philosopher phil)
     sleep(phil.eatTime);
 }
 
+// function simulates the philosopher thinking
 void think(struct philosopher phil)
 {
     phil.thinkTime = rand() % TIMEMAX + 1;

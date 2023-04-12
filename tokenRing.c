0a17b0601ed0679132afec53d682fea6fe375b9e
aswanabdi <98774646+aswanabdi@users.noreply.github.com>
2023-04-11 23:24:15 -0500

    Add files via upload

--- tokenRing.c
+++ tokenRing.c
@@ -1,156 +1,130 @@
-#include "philosophers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
 
-// states that a philosopher can be in
-enum
-{
-    THINKING,
-    HUNGRY,
-    EATING
-} state[NUMPHIL];
+typedef struct {
+    int id;
+    int has_token;
+    int eat_count;
+    int sock;
+    struct sockaddr_in prev_philosopher;
+    struct sockaddr_in next_philosopher;
+} Philosopher;
 
-// mutex and condition variables
-pthread_mutex_t mutex;
-pthread_cond_t cond_vars[NUMPHIL];
-
-// declaring arrays to store chopsticks and philosopher IDs
-int chopsticks[NUMPHIL];
-int philosopher_ids[NUMPHIL];
-
-// declare a buffer for passing tokens between philosophers and variables
-// tracks the head and tail of the buffer
-int token_buffer[NUMPHIL];
-int token_head = 0;
-int token_tail = 0;
-
-// declare an array of sockets for communication between philosophers
-int sockets[NUMPHIL];
-
-// function to initialize mutex, condition variables, and philosopher IDs
-void init()
-{
-    pthread_mutex_init(&mutex, NULL);
-    for (int i = 0; i < NUMPHIL; i++)
-    {
-        pthread_cond_init(&cond_vars[i], NULL);
-        philosopher_ids[i] = i;
-    }
-    for (int i = 0; i < NUMPHIL; i++)
-    {
-        token_buffer[i] = i;
-    }
+void think(Philosopher *philosopher) {
+    int think_time = rand() % 5 + 1;
+    printf("Philosopher %d: Thinking for %d seconds\n", philosopher->id, think_time);
+    sleep(think_time);
 }
 
-void *philosopher(void *arg)
-{
-    int id = *(int *)arg;
-    int left_chopstick = id;
-    int right_chopstick = (id + 1) % NUMPHIL;
-    while (1)
-    {
-        printf("Philosopher %d is thinking.\n", id);
-        sleep(rand() % 5); // thinking
-        pthread_mutex_lock(&mutex);
-        state[id] = HUNGRY;
-        printf("Philosopher %d is hungry and wants to eat.\n", id);
-        while (chopsticks[left_chopstick] || chopsticks[right_chopstick] || token_buffer[token_tail] != id)
-        {
-            pthread_cond_wait(&cond_vars[id], &mutex);
-        }
-        chopsticks[left_chopstick] = 1;
-        chopsticks[right_chopstick] = 1;
-        state[id] = EATING;
-        printf("Philosopher %d is eating.\n", id);
-        pthread_mutex_unlock(&mutex);
-        sleep(rand() % 5); // eating
-        pthread_mutex_lock(&mutex);
-        chopsticks[left_chopstick] = 0;
-        chopsticks[right_chopstick] = 0;
-        state[id] = THINKING;
-        printf("Philosopher %d has finished eating and is thinking.\n", id);
-        token_tail = (token_tail + 1) % (NUMPHIL + 1);
-        pthread_cond_signal(&cond_vars[token_tail]);
-        pthread_mutex_unlock(&mutex);
-    }
-    close(sockets[id]);
-    return NULL;
+void eat(Philosopher *philosopher) {
+    int eat_time = rand() % 5 + 1;
+    printf("Philosopher %d: Eating for %d seconds\n", philosopher->id, eat_time);
+    sleep(eat_time);
 }
 
-void *token_ring(void *arg)
-{
-    int id = *(int *)arg;
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVERPORT);
-    inet_pton(AF_INET, SERVERIP, &server_addr.sin_addr);
+void *philosopher_behavior(void *arg) {
+    Philosopher *philosopher = (Philosopher *)arg;
+    char token_msg[8];
 
-    // create socket
-    sockets[id] = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockets[id] < 0)
-    {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+    while (philosopher->eat_count < 1) {
+        think(philosopher);
 
-    // connect to server
-    if (connect(sockets[id], (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        perror("Connection failed");
-        exit(EXIT_FAILURE);
-    }
+        if (philosopher->has_token) {
+            printf("Philosopher %d: Picked up forks\n", philosopher->id);
+            eat(philosopher);
+            printf("Philosopher %d: Put down forks\n", philosopher->id);
 
-    int next_philosopher_id = (id + 1) % NUMPHIL; // get ID of the next philosopher
-    while (1)
-    {
-        // receive token
-        int token_received;
-        recv(sockets[id], &token_received, sizeof(token_received), 0);
+            philosopher->eat_count++;
 
-        // pass token
-        if (token_received == philosopher_ids[id])
-        {
-            printf("Philosopher %d received token.\n", id);
-            fflush(stdout);
-            token_buffer[token_head] = philosopher_ids[id];
-            token_head = (token_head + 1) % NUMPHIL;
-            pthread_cond_signal(&cond_vars[philosopher_ids[id]]);
-        }
-
-        // send token
-        if (state[next_philosopher_id] != EATING)
-        {
-            printf("Philosopher %d is sending token to philosopher %d.\n", id, next_philosopher_id);
-            fflush(stdout);
-            send(sockets[next_philosopher_id], &philosopher_ids[id], sizeof(philosopher_ids[id]), 0);
-
-            // wait for philosopher to finish eating before sending token again
-            pthread_mutex_lock(&mutex);
-            while (state[next_philosopher_id] == EATING)
-            {
-                pthread_cond_wait(&cond_vars[next_philosopher_id], &mutex);
+            // Pass the token to the next philosopher
+            strcpy(token_msg, "TOKEN");
+            sendto(philosopher->sock, token_msg, sizeof(token_msg), 0, (struct sockaddr *)&(philosopher->next_philosopher), sizeof(philosopher->next_philosopher));
+            printf("Philosopher %d: Token sent to Philosopher %d\n", philosopher->id, (philosopher->id + 1) % 5);
+            philosopher->has_token = 0;
+        } else {
+            // Receive the token
+            recvfrom(philosopher->sock, token_msg, sizeof(token_msg), 0, NULL, NULL);
+            if (strcmp(token_msg, "TOKEN") == 0) {
+                philosopher->has_token = 1;
             }
-            pthread_mutex_unlock(&mutex);
         }
-        next_philosopher_id = (next_philosopher_id + 1) % NUMPHIL; // update ID of the next philosopher
     }
-    close(sockets[id]);
+
+
+
+    printf("Philosopher %d has finished eating.\n", philosopher->id);
     return NULL;
 }
 
-// main function
-int main()
-{
-    init();
-    pthread_t philosophers[NUMPHIL];
-    pthread_t token_ring_thread;
-    for (int i = 0; i < NUMPHIL; i++)
-    {
-        pthread_create(&philosophers[i], NULL, philosopher, &philosopher_ids[i]);
+void shuffle(pthread_t *array, int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        pthread_t temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
-    pthread_create(&token_ring_thread, NULL, token_ring, &philosopher_ids[0]);
-    for (int i = 0; i < NUMPHIL; i++)
-    {
+}
+
+int main() {
+    srand(time(NULL));
+    pthread_t philosophers[5];
+    Philosopher philosopher_data[5];
+    int port_base = 5000;
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        philosopher_data[i].id = i;
+        philosopher_data[i].has_token = 0;
+        philosopher_data[i].eat_count = 0;
+
+        philosopher_data[i].sock = socket(AF_INET, SOCK_DGRAM, 0);
+        if (philosopher_data[i].sock < 0) {
+            perror("Error creating socket");
+            exit(1);
+        }
+
+        memset(&philosopher_data[i].prev_philosopher, 0, sizeof(philosopher_data[i].prev_philosopher));
+        philosopher_data[i].prev_philosopher.sin_family = AF_INET;
+        philosopher_data[i].prev_philosopher.sin_addr.s_addr = htonl(INADDR_ANY);
+        philosopher_data[i].prev_philosopher.sin_port = htons(port_base + i);
+
+                memset(&philosopher_data[i].next_philosopher, 0, sizeof(philosopher_data[i].next_philosopher));
+        philosopher_data[i].next_philosopher.sin_family = AF_INET;
+        philosopher_data[i].next_philosopher.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+        philosopher_data[i].next_philosopher.sin_port = htons(port_base + ((i + 1) % 5));
+
+        if (bind(philosopher_data[i].sock, (struct sockaddr *)&(philosopher_data[i].prev_philosopher), sizeof(philosopher_data[i].prev_philosopher)) < 0) {
+            perror("Error binding socket");
+            exit(1);
+        }
+
+        printf("Philosopher %d server connected.\n", i);
+    }
+
+    // Start philosopher threads
+    shuffle(philosophers, 5);
+    for (i = 0; i < 5; i++) {
+        pthread_create(&philosophers[i], NULL, philosopher_behavior, &philosopher_data[i]);
+    }
+
+    // Send initial token to philosopher 0
+    char initial_token_msg[8];
+    strcpy(initial_token_msg, "TOKEN");
+    sendto(philosopher_data[0].sock, initial_token_msg, sizeof(initial_token_msg), 0, (struct sockaddr *)&(philosopher_data[0].next_philosopher), sizeof(philosopher_data[0].next_philosopher));
+
+    for (i = 0; i < 5; i++) {
         pthread_join(philosophers[i], NULL);
     }
+
+    printf("All philosophers have finished eating.\n");
+
     return 0;
 }
+
